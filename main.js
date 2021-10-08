921e434be516dc016725dc0c24830aae70216c40
ArnavNikam <75774457+ArnavNikam@users.noreply.github.com>
2021-10-08 16:28:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    image_quality: 90
+});
+
+camera = document.getElementById("live");
+Webcam.attach(camera);
+
+function snapshot(){
+    Webcam.snap(function(datauri){
+        document.getElementById("snap").innerHTML = '<img id="snapshot" src"'+datauri+'">';
+    })
+}
+
+console.log("ml5 version", ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/YMyFh-nVk/",modelloaded);
+
+function modelloaded(){
+    console.log("model loaded");
+}
